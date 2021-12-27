8214b1eb35aa922458b62e6315a9de12fb904f6a
IRISPHAM3112 <92541536+IRISPHAM3112@users.noreply.github.com>
2021-12-27 23:33:42 +0700

    Change icon

--- src/java.js
+++ src/java.js
@@ -22,17 +22,18 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity")
     let windElement = document.querySelector("#wind")
     let dateElement = document.querySelector("#date")
+    let iconElement = document.querySelector("#icon")
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather.description;  
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt * 1000)
-
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
 }
 
-
 let apiKey ="bdf4089b0994f94adcf10ec4fb943bff";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
+let city = "Ha Noi"
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 axios.get(apiUrl).then(displayTemperature);
